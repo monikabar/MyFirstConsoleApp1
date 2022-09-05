fadc1afd1c0eecc3ed21ab1da72d8a65eba29aaf
Legion_Monika <monikabarkauskaite@gmail.com>
2022-09-05 22:33:28 +0300

    Klases darbas

--- MyFirstConsoleApp1/Program.cs
+++ MyFirstConsoleApp1/Program.cs
@@ -1,2 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
+Console.WriteLine("Labas, pasauli!");
+Console.WriteLine(1 + 2);
+Console.WriteLine(8 * 8 + 1);
+Console.Write("Sveiki");
+Console.Write(8 * 8 + 1);
+Console.WriteLine("Monika");
+Console.WriteLine("Rezultatas:" + 5);
+
+Console.WriteLine("Ivesk savo varda");
+var name = Console.ReadLine();
+Console.WriteLine("Mano vardas " + name + " malonu susipazinti");
+
+var digitInText = Console.ReadLine();
+var digit = int.Parse(digitInText);
+Console.WriteLine("Skaicius yra " + digit);*/
+
+/*Console.WriteLine("Kokia tavo megstama spalva?");
+var colour = Console.ReadLine();
+Console.WriteLine("Mano irgi " + colour);
+
+Console.WriteLine(17 + 23);
+
+Console.WriteLine(24 / 5);
+
+Console.WriteLine(-6 + 3 * 5);
+Console.WriteLine((13 - 2) * 7);
+Console.WriteLine((5+-2)+(20/10));*/
+
+/*Console.WriteLine("iveskite pirma skaiciu:");
+var first = Console.ReadLine();
+var firstDigit = int.Parse(first);
+Console.WriteLine("iveskite antra skaiciu:");
+var second = Console.ReadLine();
+var secondDigit = int.Parse(second);
+Console.WriteLine("sandauga:" + firstDigit*secondDigit);
+Console.WriteLine("dalyba:" +firstDigit/secondDigit);
+Console.WriteLine("suma:" + (firstDigit + secondDigit));
+Console.WriteLine("skirtumas:" + (firstDigit - secondDigit));*/
+
+/*Console.WriteLine("iveskite skaiciu");
+var number = Console.ReadLine();
+var numInt = int.Parse(number);
+for(int i=1; i <= 10; i++)
+{
+    var daugybosLentele = numInt * i;
+    Console.WriteLine(i);
+}*/
+
+/*Console.WriteLine("iveskite pirma skaiciu:");
+var first = Console.ReadLine();
+var firstDigit = int.Parse(first);
+Console.WriteLine("iveskite antra skaiciu:");
+var second = Console.ReadLine();
+var secondDigit = int.Parse(second);
+Console.WriteLine("iveskite trecia skaiciu:");
+var third = Console.ReadLine();
+var thirdDigit = int.Parse(third);
+Console.WriteLine("iveskite ketvirta skaiciu:");
+var fourth = Console.ReadLine();
+var fourthDigit = int.Parse(fourth);
+Console.WriteLine("vidurkis " + (firstDigit + secondDigit + thirdDigit + fourthDigit) / 4);*/
+
+
+/*Console.WriteLine("iveskite pirma skaiciu:");
+var first = Console.ReadLine();
+var firstDigit = int.Parse(first);
+Console.WriteLine("iveskite antra skaiciu:");
+var second = Console.ReadLine();
+var secondDigit = int.Parse(second);
+Console.WriteLine("iveskite trecia skaiciu:");
+var third = Console.ReadLine();
+var thirdDigit = int.Parse(third);
+Console.WriteLine("pirmas" + (firstDigit+secondDigit)*thirdDigit);
+Console.WriteLine("antras" + (firstDigit * thirdDigit) + (secondDigit * thirdDigit));*/
+
+
+
+
